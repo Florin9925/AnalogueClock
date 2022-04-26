3a9233975425607fb9f14333a0e0b29d60eb0618
Florin Arhip <florin9925@gmail.com>
2022-04-26 22:34:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/analogueclock/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.analogueclock.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.analogueclock.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
